c9f35812c7192262769410d6a2b6d7a86bf88e17
Sujit Thombare <itss@itss.com>
2017-08-23 16:16:12 +0530

    Read xlsx and read db

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var mongoUtils = require('./mongo-utils/mongoUtils.js');
-
+var XLSX = require('xlsx');
+var excel2Json = require('node-excel-to-json');
 
 /*
 // test for create database
@@ -15,7 +16,7 @@ mongoUtils.createDatabase('data_9234', function(err, results) {
 
 */
 
-
+/*
 //test for create collection
 mongoUtils.createCollection('data_9234', 'entities_9234', function(err, results) {
     if (err) {
@@ -26,6 +27,45 @@ mongoUtils.createCollection('data_9234', 'entities_9234', function(err, results)
     console.log(results);
 
 });
+*/
+/*
+//insert record into collection
+mongoUtils.insertJSON('test', 'emp', [{ "name": "xyz1", "age": 20 }, { "name": "xyz2", "age": 20 }, { "name": "xyz3", "age": 20 }], function(err, results) {
 
-*
-/
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log(results);
+});
+
+*/
+
+/*
+//load and insert xls into collection
+console.log('Start :' + new Date());
+
+mongoUtils.saveXLSintoCollection('test', 'products', 'C:\\Javascript\\dim_product.xlsx', function(err, results) {
+
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(results);
+    console.log('Inserted into Database :' + new Date());
+});
+
+*/
+var query = { 'P_GACactive': 'Active', 'P_CompanyCode': 'ACA' };
+var options = { 'sort': 'P_GACactive' };
+console.log('Start :' + new Date());
+mongoUtils.getRecordsFromCollection('test', 'products', query, options, function(err, result) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(result.length);
+    console.log('END :' + new Date());
+
+});
--- mongo-utils/mongoUtils.js
+++ mongo-utils/mongoUtils.js
@@ -1,5 +1,6 @@
 var jsonfile = require('jsonfile');
 var MongoClient = require('mongodb').MongoClient;
+var excel2Json = require('node-excel-to-json');
 
 
 // Read all MongoDB Connection Configuration
@@ -80,11 +81,113 @@ var createCollection = function(dbname, collectionName, callback) {
 
 }
 
+var insertJSON = function(dbname, collectionName, json, callback) {
+    if (!dbname) {
+        callback('Please enter database name.');
+        return;
+    }
+    if (!collectionName) {
+        callback('Please enter collection name.');
+        return;
+    }
+    if (!json) {
+        callback('Please enter json object.');
+        return;
+    }
+    var url = getmongoConnectionString() + dbname;
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) { callback(err); return; }
+        db.collection(collectionName).insert(json, function(err, result) {
+            if (err) { callback(err); return; }
+
+            callback(null, result);
+            db.close();
+        });
+
+
+
+
+    });
+
+}
+
+var saveXLSintoCollection = function(dbname, collectionName, xls_filename, callback) {
+        if (!dbname) {
+            callback('Please enter database name.');
+            return;
+        }
+        if (!collectionName) {
+            callback('Please enter collection name.');
+            return;
+        }
+        if (!xls_filename) {
+            callback('Please enter xls_filename.');
+            return;
+        }
+        excel2Json(xls_filename, function(err, output) {
+
+            if (err) { callback(err); return; }
+
+            insertJSON(dbname, collectionName, output[Object.keys(output)[0]], function(err, results) {
+
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                callback(null, 'Inserted into Database');
+                console.log('Inserted into Database :' + new Date());
+            });
+
+        });
+
+
+    }
+    //get recordset
+var getRecordsFromCollection = function(dbname, collectionName, query, options, callback) {
+
+
+    if (!dbname) {
+        callback('Please enter database name.');
+        return;
+    }
+    if (!collectionName) {
+        callback('Please enter collection name.');
+        return;
+    }
+
+    var url = getmongoConnectionString() + dbname;
+    MongoClient.connect(url, function(err, db) {
+        if (err) { callback(err); return; }
+        db.collection(collectionName).find(query, options).toArray(function(err, document) {
+
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            callback(null, document);
+            // console.log(document);
+
+            db.close();
+        });
+
+
+
+
+    });
+
+
+}
 
 module.exports = {
     getJSONSQL,
     getmongoConnectionString,
     createDatabase,
-    createCollection
+    createCollection,
+    insertJSON,
+    saveXLSintoCollection,
+    getRecordsFromCollection
+
 
 }
